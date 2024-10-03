c9094a50849f50ec48f135413ce5f3087b6a06ae
GiordanoGava <gavagiordano@gmail.com>
2024-10-03 19:19:11 -0300

    first commit

---
+++ src/main/java/br/univille/das2024/Das2024Application.java
@@ -0,0 +1,13 @@
+package br.univille.das2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Das2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Das2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/univille/das2024/Das2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.das2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Das2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
